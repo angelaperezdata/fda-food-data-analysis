1dd4d2934d828c7db33ba70956309504c67d7c66
Angela Perez <angela.perez.inbox@gmail.com>
2025-08-08 15:19:38 -0700

    Delete sql directory

--- sql/top_industries_by_complaints.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-products_industry_name, 
-COUNT(report_number) AS count_reports
-FROM bigquery-public-data.fda_food.food_events
-GROUP BY products_industry_name
-ORDER BY count_reports DESC
-LIMIT 10;
