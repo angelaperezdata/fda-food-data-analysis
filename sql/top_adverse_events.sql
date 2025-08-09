c5a49da330bc63751b18442ce22d08e6db667db7
Angela Perez <angela.perez.inbox@gmail.com>
2025-08-09 15:54:47 -0700

    Update top_adverse_events.sql

--- sql/top_adverse_events.sql
+++ sql/top_adverse_events.sql
@@ -1,8 +1,40 @@
+WITH cleaned_reactions AS (
+  SELECT
+    UPPER(TRIM(reaction)) AS reaction_name,
+    COUNT(*) AS total_reports
+  FROM
+    `bigquery-public-data.fda_food.food_events`,
+    UNNEST(SPLIT(reactions, ',')) AS reaction
+  WHERE
+    reaction IS NOT NULL
+    AND TRIM(reaction) != ''
+  GROUP BY
+    reaction_name
+),
+
+corrected_reactions AS (
+  SELECT
+    CASE
+      WHEN reaction_name = 'DIARRHOEA' THEN 'DIARRHEA'
+      WHEN reaction_name = 'VOMITTING' THEN 'VOMITING'
+      WHEN reaction_name = 'MALAISA' THEN 'MALAISE'       -- common typo example
+      WHEN reaction_name = 'DYSGEUSIA' THEN 'DYSGEUSIA' 
+      WHEN reaction_name = 'DYSPNOEA' THEN  'DYSPNEA'
+      WHEN REGEXP_CONTAINS(reaction_name, r'ABDOMINAL PAIN') THEN 'ABDOMINAL PAIN'
+       -- keeping medical terms as is
+      -- Add more corrections here as needed
+      ELSE reaction_name
+    END AS reaction_name,
+    total_reports
+  FROM
+    cleaned_reactions
+)
+
 SELECT
-  reactions AS reaction_name,
-  COUNT(*) AS total_reports
+  reaction_name,
+  SUM(total_reports) AS total_reports
 FROM
-  `bigquery-public-data.fda_food.food_events`
+  corrected_reactions
 GROUP BY
   reaction_name
 ORDER BY
